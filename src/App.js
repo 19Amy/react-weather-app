60073ad9ae2749cc48e6e47157f1be133b4278f9
19Amy <amygathonii@gmail.com>
2023-09-06 13:00:00 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Amy Gathoni and is {' '}
+      <div className='container'>
+
+        <h1>Weather App</h1>
+        <footer>
+           This project was coded by Amy Gathoni and is {' '}
         
-        <a href='https://github.com/19Amy/react-weather-app' target='_blank'>
-        open sourced on GitHub
-        </a>
-      </footer>
+          <a href='https://github.com/19Amy/react-weather-app' target='_blank' rel="noreferrer">
+             open sourced on GitHub
+          </a>
+        
+        </footer>
+
+      </div>
+      
       
     </div>
   );
