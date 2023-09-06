efcbc9a848ce750cd43c8c9fa06a7a649bb9e924
19Amy <amygathonii@gmail.com>
2023-09-06 11:03:37 +0300

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Amy Gathoni and is {' '}
+        
+        <a href='https://github.com/19Amy/react-weather-app' target='_blank'>
+        open sourced on GitHub
         </a>
-      </header>
+      </footer>
+      
     </div>
   );
 }
