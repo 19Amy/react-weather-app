4ea5ca77d1778f2386c637f67c3a1868c584bab6
19Amy <amygathonii@gmail.com>
2023-09-30 16:43:32 +0300

    added Fahrenheit units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props){
     return(
@@ -21,8 +22,8 @@ export default function WeatherInfo(props){
                   
                 </div>
                 <div className="col-6 units-section">
-                  <span className="temperature">{Math.round(props.data.temperature)}</span> 
-                  <span className="units"> °C </span>
+                  <WeatherTemperature celsius={props.data.temperature}/>
+                  
                 </div>
               </div>
                
