71e957bdcd0b5137e4b47b26eb4689dde7335b52
19Amy <amygathonii@gmail.com>
2023-10-03 10:23:09 +0300

    added WeatherForecast component
    added the forecast layout

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -28,7 +28,7 @@ export default function WeatherIcon(props){
             <ReactAnimatedWeather
                icon={codeMapping[props.code]}
                color="Purple"
-              size={64}
+              size={props.size}
                animate={true}
              />
         )
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props){
               <div className="row img-row">
                 <div className="col-6 img-component" >
                   {/**Weather image component in line below*/}
-                  <WeatherIcon code={props.data.icon} />
+                  <WeatherIcon code={props.data.icon} size={60} />
                   
                 </div>
                 <div className="col-6 units-section">
