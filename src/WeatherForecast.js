67fbd31678a25b67f512ac76f9b6e98ecf9fdaec
19Amy <amygathonii@gmail.com>
2023-10-03 14:44:42 +0300

    Made waether forecast update when you change city

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css"
@@ -7,6 +7,11 @@ import axios from "axios";
 export default function WeatherForecast(props){
     let[loaded, setLoaded]=useState(false);
     let[forecast, setForecast]=useState(null);
+
+    useEffect(() =>{
+        setLoaded(false);
+    },([props.coordinates]));
+
 
     function handleResponse(response){
           console.log(response.data)
@@ -19,6 +24,18 @@ export default function WeatherForecast(props){
         return(
             <div className="WeatherForecast">
                 <div className="row">
+                  
+                {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else{
+              return null;
+            }
+          })}
                   <div className ="col">
                     <WeatherForecastDay data={forecast.list[0]}/>
                   </div>
