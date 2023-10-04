8801a25d76ad63395985cae913e8d2fa430dc1b4
19Amy <amygathonii@gmail.com>
2023-10-04 11:30:06 +0300

    removed console log

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -14,7 +14,7 @@ export default function WeatherForecast(props){
 
 
     function handleResponse(response){
-          console.log(response.data.daily)
+          
           setForecast(response.data.daily)
           setLoaded(true);
     }
