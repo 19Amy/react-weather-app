c349228e42eb5d8b33fda11aa84d0b59eaadbb44
19Amy <amygathonii@gmail.com>
2023-10-04 11:27:14 +0300

    made weather forecast more accurat

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -14,8 +14,8 @@ export default function WeatherForecast(props){
 
 
     function handleResponse(response){
-          
-          setForecast(response.data)
+          console.log(response.data.daily)
+          setForecast(response.data.daily)
           setLoaded(true);
     }
 
@@ -27,22 +27,22 @@ export default function WeatherForecast(props){
                   
                
                   <div className ="col">
-                    <WeatherForecastDay data={forecast.list[0]}/>
+                    <WeatherForecastDay data={forecast[1]}/>
                   </div>
                   <div className ="col">
-                    <WeatherForecastDay data={forecast.list[3]}/>
+                    <WeatherForecastDay data={forecast[2]}/>
                   </div>
                   <div className ="col">
-                    <WeatherForecastDay data={forecast.list[11]}/>
+                    <WeatherForecastDay data={forecast[3]}/>
                   </div>
                   <div className ="col">
-                    <WeatherForecastDay data={forecast.list[19]}/>
+                    <WeatherForecastDay data={forecast[4]}/>
                   </div>
                   <div className ="col">
-                    <WeatherForecastDay data={forecast.list[27]}/>
+                    <WeatherForecastDay data={forecast[5]}/>
                   </div>
                   <div className ="col">
-                    <WeatherForecastDay data={forecast.list[35]}/>
+                    <WeatherForecastDay data={forecast[6]}/>
                   </div>
                 </div>
     
@@ -53,7 +53,7 @@ export default function WeatherForecast(props){
          let apiKey ="93d43dfe3b4a950e5b187e5dc313705e";
         let longitude=props.coordinates.lon;
         let latitude =props.coordinates.lat;
-        let apiUrl=`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     
         axios.get(apiUrl).then(handleResponse);
         return null;
